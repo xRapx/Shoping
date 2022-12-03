bca11fe049e7d3365642d54100a09a3b0ce24b57
xRapx <huynhminhquan2706@gmail.com>
2022-12-03 08:56:50 +0700

    ok

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./page/App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Context from './context/Context';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Context>
       <App />
-    </Router>
+    </Context>
+   
   </React.StrictMode>
 );
 
